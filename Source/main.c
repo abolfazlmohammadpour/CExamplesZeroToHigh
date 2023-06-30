0207c7dcc53296ad4ba83228bb97f4433e6e96f8
AbolfazlMohammadpour <abolfazlmohammadpourqaemshahr@gmail.com>
2023-06-30 09:50:39 +0330

    Edit <Source/main.c>

--- Source/main.c
+++ Source/main.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+void ClearInputBuffer(void);
 
 int main(int argc, const char *argv[])
 {
-    return 0;
+    long long int UserNumber = (long long int)0;
+
+    while (true)
+    {
+        printf("%s", "Please Enter Your Number : ");
+
+        int ResultOfReading = (int)0;
+        ResultOfReading = (int)scanf("%lld", &UserNumber);
+
+        if (ResultOfReading == (int)1)
+        {
+            break;
+        }
+        else
+        {
+            printf("%c%s%c", '\a', "Error: Please Just Enter A Number.", '\n');
+            ClearInputBuffer();
+
+            continue;
+        }
+    }
+
+    if ((UserNumber % (long long int)2) == (long long int)0)
+        printf("%s%c", "Your Number Is Even", '\n');
+    else
+        printf("%s%c", "Your Number Is Odd", '\n');
+
+    return (int)0;
+}
+
+void ClearInputBuffer(void)
+{
+    int Character = (int)0;
+
+    while (true)
+    {
+        Character = (int)getchar();
+
+        if ((Character != (int)'\n') && (Character != (int)EOF))
+            continue;
+        else
+            break;
+    }
 }
